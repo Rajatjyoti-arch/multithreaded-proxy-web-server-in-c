69816a72f6b1b68828408612977e0c88a84661ee
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-21 15:18:52 +0530

    Integrate: Added/Modified functions: int  add_cache_element, void remove_cache_element; Updated sock

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
+
+#define Max_Clients 10
 
 typedef struct cache_element cache_element;
 
@@ -14,3 +17,8 @@ struct cache_element{
 };
 cache_element* find(char* url);
 int  add_cache_element(char* url, char* data, int size); 
+void remove_cache_element();
+
+int port_number = 8080;
+int proxy_socketID;
+pthread_t tid[Max_Clients];
