b54cc57a2233bef9b337bdaabdfeb9092799e6fb
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-24 17:12:21 +0530

    Implement: Updated socket connections/network layer; Updated HTTP parsing/proxy flow

--- main.c
+++ main.c
@@ -104,7 +104,11 @@ int handle_request(int clientSocketID, ParsendRequest *request, char* tempReq){
     bytes_send = recv(remoteSocketID, buf, max_bytes-1, 0);
     char * temp_buffer = (char*)malloc(sizeof(char)*max_bytes);
     int temp_buffer_size = max_bytes;
-    
+    int temp_buffer_index = 0;
+
+    while(bytes_send > 0){
+        bytes_Send = send(clientSocketID, buf, bytes_Send, 0)
+    }
 }
 
 void* thread_fn(void* socketNew){
