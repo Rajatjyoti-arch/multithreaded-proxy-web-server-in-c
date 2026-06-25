8175c49f5a793955709fef05f3413e194a9b5a39
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-25 23:58:17 +0530

    Streamline: Updated thread management/synchronization

--- main.c
+++ main.c
@@ -376,7 +376,7 @@ void remove_cache_element(){
     int temp_lock_val = pthread_mutex_lock(&lock);
     printf("Lock is aqquired\n");
     if(head != NULL){
-        for(q = head, p = head, temp =  )
+        for(q = head, p = head, temp = head; q -> next != NULL; q = q  )
     }
 }
 
