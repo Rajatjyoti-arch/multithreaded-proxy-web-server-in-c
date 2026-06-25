910a0d033a7eafc367423604ba95da85d731fda9
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-25 23:39:56 +0530

    Adjust: Updated thread management/synchronization

--- main.c
+++ main.c
@@ -369,6 +369,7 @@ cache_element *find(char* url){
 int add_cache_element(char *data, int size, char *url){
     int temp_lock_val = pthread_mutex_lock(&lock);
     printf("Add cache lock Acquired %d\n", temp_lock_val);
-    int element_size = size + 1 + strlen(url) + sizeof(cache_elements);
+    int element_size = size + 1 + strlen(url) + sizeof(cache_element);
+    if(element_size < MAX_ELEMENT_)
 
 }
