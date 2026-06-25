5dc6b2b1c1779d94ae619119e5835d058df3a82c
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-25 23:38:53 +0530

    Optimize: Updated thread management/synchronization

--- main.c
+++ main.c
@@ -368,6 +368,7 @@ cache_element *find(char* url){
 
 int add_cache_element(char *data, int size, char *url){
     int temp_lock_val = pthread_mutex_lock(&lock);
-    printf("Add cache lock Acquired ")
+    printf("Add cache lock Acquired %d\n", temp_lock_val);
+    int element_size = size + 1
 
 }
